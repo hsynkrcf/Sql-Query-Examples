c9f77e19754bd273e274638c68a53cf336a28af1
Hüseyin Karacif <hsynkrcf@gmail.com>
2019-09-01 02:31:28 +0300

    Add files via upload

---
+++ Examples.sql
@@ -0,0 +1,113 @@
+﻿-- Hangi tedarikçiden hangi ürünü kaç adet  temin etmişim? Tedarikci | UrunAdedi
+SELECT s.CompanyName AS Tedarikçi,ProductName AS ÜrünAdı,SUM(p.UnitsInStock + p.UnitsOnOrder + od.Quantity) AS [Toplam Ürün Adedi]
+FROM Products p
+	JOIN Suppliers s
+		ON p.SupplierID = s.SupplierID
+	JOIN [Order Details] od
+		ON p.ProductID = od.ProductID
+GROUP BY s.CompanyName,p.ProductName 
+
+-- Nancy adındaki personelim hangi firmaya toplam kaç adet ürün satmıştır? FirmaAdi | UrunAdet
+SELECT c.CompanyName AS FirmaAdi,SUM(od.Quantity)AS UrunAdet
+FROM Employees e
+	JOIN Orders o
+		ON e.EmployeeID = o.EmployeeID
+	JOIN [Order Details] od
+		ON o.OrderID = od.OrderID
+	JOIN Customers c
+		ON c.CustomerID = o.CustomerID
+GROUP BY c.CompanyName
+
+-- Batı bölgesinden sorumlu çalışanlara ait müşteri sayısı bilgisini getirin. Calisan | MusteriSayisi
+SELECT e.FirstName+' '+e.LastName AS İsimSoyisim,COUNT(DISTINCT o.CustomerID) AS MüşteriSayisi
+FROM Employees e
+	JOIN EmployeeTerritories et
+		ON et.EmployeeID = e.EmployeeID
+	JOIN Territories t
+		ON t.TerritoryID = et.TerritoryID
+	JOIN Orders o 
+		ON o.EmployeeID = et.EmployeeID
+		
+WHERE t.RegionID = 2
+GROUP BY e.FirstName+' '+e.LastName
+
+-- Kategori adı Confections olan ürünleri hangi ülkelere fiyat olarak toplam ne kadar gönderdik ? Ulke | ToplamFiyat
+SELECT ShipCountry AS ÜLKE,SUM(p.UnitPrice) AS ToplamFiyat
+FROM Products p
+	JOIN Categories c
+		ON p.CategoryID = c.CategoryID
+	JOIN [Order Details] od
+		ON p.ProductID = od.ProductID
+	JOIN Orders o
+		ON o.OrderID = od.OrderID
+WHERE CategoryName = 'confections'
+GROUP BY  o.ShipCountry
+
+-- Her bir ürün için ortalama talep sayısı(ortalama sipariş adeti) bilgisini ürün adıyla beraber listeleyin. UrunAdi | OrtalamaTalepSayısı
+SELECT P.ProductName AS UrunAD,AVG(od.Quantity) AS OrtalamaTalepSayısı
+FROM Products p
+	JOIN [Order Details] od
+		ON p.ProductID = od.ProductID
+GROUP BY p.ProductName
+
+-- 250'den fazla sipariş taşımış olan kargo firmalarının adlarını, telefon numaralarını ve taşıdıkları sipariş sayılarını getiren sorguyu yazın. FirmaAdi | Telefon | SiparisSayisi
+SELECT s.CompanyName,s.Phone,COUNT(*) AS TaşınanKargo
+FROM Orders o
+	JOIN Shippers s
+		ON s.ShipperID = o.ShipVia
+GROUP BY s.CompanyName,s.Phone
+HAVING COUNT(*) > 250
+
+-- Müşterilerimin toplam sipariş adetlerini Müşteri adı ile birlikte raporlayın	CustomerName | TotalOrdersCount
+SELECT c.CompanyName,COUNT(*) AS TotalOrdersCount
+FROM Orders o
+	RIGHT JOIN Customers c
+		ON c.CustomerID = o.CustomerID
+GROUP BY c.CompanyName
+
+-- Kargo şirketlerine göre taşınan sipariş sayıları nedir? ShipperName | TotalOrdersCount
+SELECT s.CompanyName,COUNT(*) AS TaşınanKargo
+FROM Orders o
+	JOIN Shippers s
+		ON s.ShipperID = o.ShipVia
+GROUP BY s.CompanyName
+
+-- Ürün Id ve isimlerini, bugünkü fiyatı ile birlikte bugüne kadar yer aldığı siparişlerdeki en ucuz fiyat ve bu fiyat ile arasındaki farkı da hesaplayarak listeleyin. ProductID | ProductName | UnitPrice | LowestPrice | Difference
+SELECT DISTINCT p.ProductID,p.ProductName,p.UnitPrice,MIN(od.UnitPrice) AS LowestPrice ,p.UnitPrice-MIN(od.UnitPrice) AS Difference
+FROM Products p
+	JOIN [Order Details] od
+		ON p.ProductID = od.ProductID
+GROUP BY p.ProductID,p.ProductName,p.UnitPrice
+
+-- Sevilla şehri hariç ıspanyaya gönderilen kargoların toplam adedi, toplam tutarı ve ortalama tutarını şehirlere göre raporlayın. City | TotalCount | TotalPrice | Average
+SELECT o.ShipCity AS City,SUM(od.UnitPrice*od.Quantity*(1-Discount)) TotalPrice,AVG(od.UnitPrice*od.Quantity*(1-Discount)) AS Average
+FROM Orders o
+	JOIN [Order Details] od
+		ON od.OrderID = o.OrderID
+WHERE o.ShipCity NOT LIKE 'Sevilla' AND o.ShipCountry LIKE 'Spain'
+GROUP BY o.ShipCity
+
+-- Her yıl hangi ülkeye kaç adet sipariş göndermişim? Year | Country | TotalOrdersCount
+SELECT YEAR(o.OrderDate) AS Yil,
+	   o.ShipCountry AS Country,
+	   COUNT(o.OrderID) AS SiparisAdedi
+FROM [Order Details] od
+	JOIN Orders o
+		ON od.OrderID = o.OrderID
+GROUP BY YEAR(o.OrderDate),o.ShipCountry
+
+-- En değerli müşterim kim? (Bana en çok para kazandıran)
+SELECT TOP 1 c.CompanyName AS Musterim,
+       SUM(od.UnitPrice * od.Quantity * (1 - od.Discount)) AS Ciro
+FROM [Order Details] od
+	JOIN Orders o
+		ON od.OrderID = o.OrderID
+	JOIN Customers c
+		ON o.CustomerID = c.CustomerID
+GROUP BY c.CompanyName
+ORDER BY Ciro DESC
+
+-- şehir bazında sipariş adetlerim nelerdir? City | Count
+SELECT o.ShipCity AS City,COUNT(*) AS SiparişAdet
+FROM Orders O
+GROUP BY o.ShipCity
---
+++ Triggers.sql
@@ -0,0 +1,107 @@
+﻿-- 10 siparişten sonra müşteriye aldığı ürünlerde %10 indirim uygulasın.
+GO
+
+CREATE TRIGGER trg_IndirimUygula
+ON Orders
+AFTER INSERT
+AS
+BEGIN
+	DECLARE @musteriIDSayisi int
+	DECLARE @musteriID int
+	DECLARE @siparisID int
+
+	SELECT @musteriID=CustomerID,
+		   @siparisID=OrderID,
+		   @productID=ProductID
+	 from inserted
+
+	SELECT @musteriIDSayisi=COUNT(*) from Orders	
+	WHERE CustomerID = @musteriID
+GROUP BY CustomerID
+
+	IF @musteriIDSayisi>10
+	BEGIN
+
+		INSERT INTO Orders(OrderID,CustomerID,OrderDate)
+		VALUES(@siparisID,@musteriID,GETDATE())
+
+
+		INSERT INTO [Order Details](OrderID,Discount)
+		VALUES(@siparisID,'0,10')
+
+	END
+	ELSE
+	BEGIN
+		INSERT INTO Orders(OrderID,CustomerID,OrderDate)
+		VALUES(@siparisID,@musteriID,GETDATE())
+	END
+END
+
+GO
+-- Sipariş verilirken, siparişte yer alan ürünlerden herhangi biri kritik seviyenin altına düşüyorsa siparişi kaydetmeyip uyarı versin.
+
+CREATE TRIGGER trg_KritikSeviyeUyarı
+ON Orders
+INSTEAD OF INSERT
+AS
+BEGIN
+	DECLARE @KritikSeviye int
+	DECLARE @Urun NVARCHAR(50)
+
+	SELECT  @KritikSeviye=(SELECT ReorderLevel FROM Products)
+
+	
+
+END
+
+
+
+-- Verilen siparişteki bir ürünün adet bilgisi yanlış girilmiş ve düzeltilmek istendiğinde o ürünün stok bilgisini de düzeltsin.
+GO
+
+CREATE TRIGGER trg_StokDuzeltme
+ON [Order Details]
+AFTER INSERT
+AS
+BEGIN
+	DECLARE @UrunAdet int
+	DECLARE @UrunStok int
+	DECLARE @UrunAdi nvarchar(50)
+	DECLARE @SiparisID int
+
+	SELECT @SiparisID=OrderID FROM inserted
+	SELECT
+
+END
+
+
+-- Bir kategoriye 15 ten fazla ürün eklenemesin.
+CREATE TRIGGER trg_UrunEklemeKısıt
+ON Products
+INSTEAD OF INSERT
+AS
+BEGIN
+	DECLARE @kategoriID int
+	DECLARE @UrunAdi nvarchar(max)
+	DECLARE @kategoriAdet int
+	DECLARE @discontinued AS BIT
+
+	
+	SELECT @kategoriID=CategoryID,
+		   @UrunAdi=ProductName
+	From inserted
+
+	SELECT @kategoriAdet=COUNT(*) from Products	
+	WHERE CategoryID = @kategoriID
+GROUP BY CategoryID
+
+	IF @kategoriAdet>=15
+	BEGIN
+	PRINT '15 adet üründen fazla ürün girilemez'
+	END
+	ELSE
+	BEGIN
+		INSERT INTO Products(ProductName,CategoryID,Discontinued)
+		VALUES(@UrunAdi,@kategoriID,@discontinued)
+	END
+END
